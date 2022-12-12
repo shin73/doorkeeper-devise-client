945698d9dde21cc89b43f5a56c580d54e64d49eb
shin73 <yanshi7311@gmail.com>
2022-12-13 00:14:08 +0900

    Basic Auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def doorkeeper
+    binding.pry
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       @user.update_doorkeeper_credentials(request.env["omniauth.auth"])
--- lib/omniauth/strategies/doorkeeper.rb
+++ lib/omniauth/strategies/doorkeeper.rb
@@ -5,7 +5,12 @@ module OmniAuth
 
       option :client_options,
              site: ENV["DOORKEEPER_APP_URL"],
-             authorize_path: "/oauth/authorize"
+             authorize_path: "/oauth/authorize",
+             connection_opts: {
+               headers: {
+                 'Authorization' => "Basic #{ENV["BASIC_AUTHORIZATION_TOKEN"]}"
+               }
+             }
 
       uid do
         raw_info["id"]
@@ -20,9 +25,8 @@ module OmniAuth
       end
 
       def raw_info
-        # p @raw_info
-        # p @raw_info['extra'] if @raw_info.present?
-        @raw_info ||= access_token.get("/user_info").parsed
+        access_token_basic_auth = ::OAuth2::AccessToken.from_hash(access_token.client, { access_token: ENV["BASIC_AUTHORIZATION_TOKEN"], header_format: 'Basic %s' })
+        @raw_info ||= access_token_basic_auth.get("/user_info?access_token=#{access_token.token}").parsed
       end
     end
   end
